8346689199b3e156f489954912645bde6c8c580d
linspisokobhod <linosnext@gmail.com>
2026-08-28 11:32:03 +0300

    Update parser[#LSO-#LinSpisokObhod].py

--- parser[#LSO-#LinSpisokObhod].py
+++ parser[#LSO-#LinSpisokObhod].py
@@ -36,7 +36,8 @@ SOURCES = [
     "https://mifa.world/bingo",
     "https://raw.githubusercontent.com/ksenkovsolo/HardVPN-bypass-WhiteLists-/refs/heads/main/vpn-lte/WHITELIST-ALL.txt",
     "https://raw.githubusercontent.com/RKPchannel/RKP_bypass_configs/refs/heads/main/blacklist.txt",
-    "https://raw.githubusercontent.com/RKPchannel/RKP_bypass_configs/refs/heads/main/whitelist.txt"
+    "https://raw.githubusercontent.com/RKPchannel/RKP_bypass_configs/refs/heads/main/whitelist.txt",
+    "https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt"
 ]
 
 REQUEST_TIMEOUT = 15
